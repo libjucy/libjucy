c25b78f3984600db0d64d626faf5c8282e0ea0d7
Anupam Basak <anupam.basak27@gmail.com>
2024-09-11 14:04:17 +0530

    Remove unnecessary QT Dependencies

--- cmake/Shiboken2Helper.cmake
+++ cmake/Shiboken2Helper.cmake
@@ -4,7 +4,4 @@ find_path(Shiboken2_INCLUDE_BASEDIR include/shiboken2)
 set(Shiboken2_INCLUDE_DIRS
         ${Shiboken2_INCLUDE_BASEDIR}/include/shiboken2
         ${Qt5Core_INCLUDE_DIRS}
-        ${Qt5Gui_INCLUDE_DIRS}
-        ${Qt5Network_INCLUDE_DIRS}
-        ${Qt5Qml_INCLUDE_DIRS}
 )
